07da2fddbf82efd8da557cf582927ddb1bb8ab62
aori111 <aori.do111@gmail.com>
2025-10-07 12:18:59 +0700

    first commit

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -2,175 +2,104 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-/** 
- * @title Ballot
- * @dev Implements voting process along with vote delegation
- */
-contract Ballot {
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
-    struct Voter {
-        uint weight; // weight is accumulated by delegation
-        bool voted;  // if true, that person already voted
-        address delegate; // person delegated to
-        uint vote;   // index of the voted proposal
+contract ToDoList {
+    // Address owner
+    address assigner;
+
+    enum STATUS {
+        PENDING,
+        COMPLETED,
+        OVERDUE
     }
 
-    // This is a type for a single proposal.
-    struct Proposal {
-        // If you can limit the length to a certain number of bytes, 
-        // always use one of bytes1 to bytes32 because they are much cheaper
-        bytes32 name;   // short name (up to 32 bytes)
-        uint voteCount; // number of accumulated votes
+    struct Task {
+        string title;
+        string task;
+        uint256 createdAt;
+        uint256 deadline;
+        STATUS status;
     }
 
-    address public chairperson;
+    mapping (address => Task[]) private tasks;
 
-    // This declares a state variable that
-    // stores a 'Voter' struct for each possible address.
-    mapping(address => Voter) public voters;
+    event TaskAdded(address indexed  assignee, uint256 indexed taskId);
+    event TaskCompleted(address indexed assignee, uint256 indexed taskId);
+    event TaskOverdue(address indexed assignee, uint256 indexed taskId);
+    event TaskDeleted(address indexed assignee, uint256 indexed taskId);
 
-    // A dynamically-sized array of 'Proposal' structs.
-    Proposal[] public proposals;
+    // Pakai calldata untuk hemat gas external function
+    function addTask(string calldata _title, string calldata _task, uint256 _deadline) external {
+        require(bytes(_title).length > 0, "Title must be provided");
+        require(_deadline > block.timestamp, "Deadline must be in the future");
 
-    /** 
-     * @dev Create a new ballot to choose one of 'proposalNames'.
-     * @param proposalNames names of proposals
-     */
-    constructor(bytes32[] memory proposalNames) {
-        chairperson = msg.sender;
-        voters[chairperson].weight = 1;
+        tasks[msg.sender].push(Task({
+            title: _title,
+            task: _task,
+            createdAt: block.timestamp,
+            deadline: _deadline,
+            status: STATUS.PENDING
+        }));
 
-        // For each of the provided proposal names,
-        // create a new proposal object and add it
-        // to the end of the array.
-        for (uint i = 0; i < proposalNames.length; i++) {
-            // 'Proposal({...})' creates a temporary
-            // Proposal object and 'proposals.push(...)'
-            // appends it to the end of 'proposals'.
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
-        }
+        uint256 taskId = tasks[msg.sender].length - 1;
+        emit TaskAdded(msg.sender, taskId);
     }
 
-     /** 
-     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
-     * @param voter address of voter
-     */
-    function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to 'false', execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use 'require' to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
-        require(voters[voter].weight == 0, "Voter already has the right to vote.");
-        voters[voter].weight = 1;
-    }
+    function completeTask(uint256 _taskId) external validTaskId(_taskId) {
 
-    /**
-     * @dev Delegate your vote to the voter 'to'.
-     * @param to address to which vote is delegated
-     */
-    function delegate(address to) external {
-        // assigns reference
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "You have no right to vote");
-        require(!sender.voted, "You already voted.");
+        Task storage task = tasks[msg.sender][_taskId];
 
-        require(to != msg.sender, "Self-delegation is disallowed.");
-
-        // Forward the delegation as long as
-        // 'to' also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
-
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
+        require(task.status == STATUS.PENDING, "Task must be Pending");
+        
+        if (block.timestamp > task.deadline) {
+            task.status = STATUS.OVERDUE;
+            emit TaskOverdue(msg.sender, _taskId);
         }
 
-        Voter storage delegate_ = voters[to];
+        task.status = STATUS.COMPLETED;
+        emit TaskCompleted(msg.sender, _taskId);
+    }
 
-        // Voters cannot delegate to accounts that cannot vote.
-        require(delegate_.weight >= 1);
+        // Fungsi untuk menandai tugas sebagai overdue (jika deadline telah lewat)
+    // - Validasi status masih pending
+    // - Validasi block.timestamp > deadline
+    // - Ubah status ke Overdue
+    // - Emit TaskOverdue
 
-        // Since 'sender' is a reference, this
-        // modifies 'voters[msg.sender]'.
-        sender.voted = true;
-        sender.delegate = to;
+    function setOverdueTask(uint256 _taskId) external validTaskId(_taskId) {
+        Task storage task = tasks[msg.sender][_taskId];
 
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
-            delegate_.weight += sender.weight;
+        require(task.status == STATUS.PENDING, "Task must be Pending");
+        require(task.deadline < block.timestamp, "Task is not overdue yet");
+
+        task.status = STATUS.OVERDUE;
+        emit TaskOverdue(msg.sender, _taskId);
+    }
+
+
+    function deleteTask(uint256 _taskId) external validTaskId(_taskId) {
+        uint256 taskCount = tasks[msg.sender].length;
+
+        if (_taskId != taskCount - 1) {
+            tasks[msg.sender][_taskId] = tasks[msg.sender][taskCount - 1];
         }
+
+        tasks[msg.sender].pop();
+
+        emit TaskDeleted(msg.sender, _taskId);
     }
 
-    /**
-     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
-     * @param proposal index of proposal in the proposals array
-     */
-    function vote(uint proposal) external {
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "Has no right to vote");
-        require(!sender.voted, "Already voted.");
-        sender.voted = true;
-        sender.vote = proposal;
-
-        // If 'proposal' is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
-        proposals[proposal].voteCount += sender.weight;
+    function getTasks() public view returns (Task[] memory) {
+        return tasks[msg.sender];
     }
 
-    /** 
-     * @dev Computes the winning proposal taking all previous votes into account.
-     * @return winningProposal_ index of winning proposal in the proposals array
-     */
-    function winningProposal() public view
-            returns (uint winningProposal_)
-    {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
-            }
-        }
+    // Fungsi untuk mendapatkan jumlah tugas user (optional untuk front-end)
+    
+    function getTaskCount() public view returns (uint) {
+        return tasks[msg.sender].length;
     }
 
-    /** 
-     * @dev Calls winningProposal() function to get the index of the winner contained in the proposals array and then
-     * @return winnerName_ the name of the winner
-     */
-    function winnerName() external view
-            returns (bytes32 winnerName_)
-    {
-        winnerName_ = proposals[winningProposal()].name;
+    modifier validTaskId(uint256 _taskId) {
+        require(_taskId < tasks[msg.sender].length, "Invalide task ID");
+        _;
     }
 }
