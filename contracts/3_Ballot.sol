bb5e5b6047526f3b1999519b632c7d42dddf8f72
aori111 <aori.do111@gmail.com>
2025-10-11 21:32:11 +0700

    Create necessary variable, ToDo structure, status variable, constructor, assignTask and reassignTask function, onlyAssigner modifier

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -3,119 +3,62 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract ToDoList {
-    // Address owner
     address assigner;
-    mapping (address => uint) public addressToTaskCount;
+    ToDo[] toDos;
+    mapping (address => uint) addressToTaskCount;
+
+    struct ToDo {
+        uint taskId;
+        address assignee;
+        string task;
+        uint32 deadline;
+        STATUS status;
+    }
+
+    enum STATUS {
+        INCOMPLETE,
+        COMPLETED,
+        FAILED
+    }
+
+    event TaskAdded(ToDo);
 
     constructor() {
         assigner = msg.sender;
         addressToTaskCount[msg.sender] = 1;
     }
 
-    enum STATUS {
-        PENDING,
-        COMPLETED,
-        OVERDUE
+    function assignTask(address _assignee, string calldata _task, uint32 _deadline) external onlyAssigner {
+        ToDo memory newTask = ToDo(
+            uint(toDos.length),
+            _assignee,
+            _task,
+            _deadline,
+            STATUS.INCOMPLETE
+        );
+        toDos.push(newTask);
+
+        addressToTaskCount[_assignee]++;
+
+        emit TaskAdded(newTask);
     }
 
-    struct Task {
-        string title;
-        string task;
-        uint256 createdAt;
-        uint256 deadline;
-        STATUS status;
+    function reassignTask(uint _taskId, address _assignee) external onlyAssigner {
+        // Status tasknya harus belum selesai
+        require(toDos[_taskId].status == STATUS.INCOMPLETE, "This task is already completed.");
+        // Address assignee tidak boleh previous assignee
+        require(toDos[_taskId].assignee != _assignee, "Task can't be assigned to same address");
+
+        // Kurangin jumlah task address ini
+        addressToTaskCount[toDos[_taskId].assignee]--;
+        // Ubah address assignne task ini menjadi address assignne yang baru
+        toDos[_taskId].assignee = _assignee;
+        // Tambah jumlah task assignee baru
+        addressToTaskCount[_assignee]++;
     }
 
-    mapping (address => Task[]) private tasks;
-
-    event TaskAdded(address indexed  assignee, uint256 indexed taskId);
-    event TaskCompleted(address indexed assignee, uint256 indexed taskId);
-    event TaskOverdue(address indexed assignee, uint256 indexed taskId);
-    event TaskDeleted(address indexed assignee, uint256 indexed taskId);
-
-    // Pakai calldata untuk hemat gas external function
-    function addTask(string calldata _title, string calldata _task, uint256 _deadline) external {
-        require(bytes(_title).length > 0, "Title must be provided");
-        require(_deadline > block.timestamp, "Deadline must be in the future");
-
-        tasks[msg.sender].push(Task({
-            title: _title,
-            task: _task,
-            createdAt: block.timestamp,
-            deadline: _deadline,
-            status: STATUS.PENDING
-        }));
-
-        uint256 taskId = tasks[msg.sender].length - 1;
-        emit TaskAdded(msg.sender, taskId);
-    }
-
-    function completeTask(uint256 _taskId) external validTaskId(_taskId) {
-
-        Task storage task = tasks[msg.sender][_taskId];
-
-        require(task.status == STATUS.PENDING, "Task must be Pending");
-        
-        if (block.timestamp > task.deadline) {
-            task.status = STATUS.OVERDUE;
-            emit TaskOverdue(msg.sender, _taskId);
-        }
-
-        task.status = STATUS.COMPLETED;
-        emit TaskCompleted(msg.sender, _taskId);
-    }
-
-        // Fungsi untuk menandai tugas sebagai overdue (jika deadline telah lewat)
-    // - Validasi status masih pending
-    // - Validasi block.timestamp > deadline
-    // - Ubah status ke Overdue
-    // - Emit TaskOverdue
-
-    function setOverdueTask(uint256 _taskId) external validTaskId(_taskId) {
-        Task storage task = tasks[msg.sender][_taskId];
-
-        require(task.status == STATUS.PENDING, "Task must be Pending");
-        require(task.deadline < block.timestamp, "Task is not overdue yet");
-
-        task.status = STATUS.OVERDUE;
-        emit TaskOverdue(msg.sender, _taskId);
-    }
-
-
-    function deleteTask(uint256 _taskId) external validTaskId(_taskId) {
-        uint256 taskCount = tasks[msg.sender].length;
-
-        if (_taskId != taskCount - 1) {
-            tasks[msg.sender][_taskId] = tasks[msg.sender][taskCount - 1];
-        }
-
-        tasks[msg.sender].pop();
-
-        emit TaskDeleted(msg.sender, _taskId);
-    }
-
-    function getTasks() public view returns (Task[] memory) {
-        return tasks[msg.sender];
-    }
-
-    // Fungsi untuk mendapatkan jumlah tugas user (optional untuk front-end)
-    
-    function getTaskCount() public view returns (uint) {
-        return tasks[msg.sender].length;
-    }
-
-    modifier onlyAssigner {
-        require(msg.sender == assigner, "Only assigner");
-        _;
-    }
-
-    modifier onlyAssignee {
-        require(addressToTaskCount[msg.sender] != 0, "Only assignee");
-        _;
-    }
-
-    modifier validTaskId(uint256 _taskId) {
-        require(_taskId < tasks[msg.sender].length, "Invalide task ID");
+    modifier onlyAssigner() {
+        require(msg.sender == assigner, "This function can be only execute by Assigner, address is not assigner.");
         _;
     }
 }
